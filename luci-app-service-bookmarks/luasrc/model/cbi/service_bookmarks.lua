e00dc0c656575b944097d38d6c0c5888ca5f8e98
sveil <support@sveil.com>
2025-05-12 17:41:15 +0800

    fix service_bookmarks #11

--- luci-app-service-bookmarks/luasrc/model/cbi/service_bookmarks.lua
+++ luci-app-service-bookmarks/luasrc/model/cbi/service_bookmarks.lua
@@ -8,24 +8,10 @@ t.template = "cbi/tblsection"
 t.anonymous = true
 t.addremove = true
 
-e = t:option(DummyValue, "bookmarks_status", translate("Service BookMarks"))
-e.template = "service_bookmarks/service_bookmarks"
-e.value = translate("Collecting data...")
-
 e = t:option(Value, "title", translate("Service Title"))
 e.rmempty = true
 
 e = t:option(Value, "url", translate("Url Address"))
-e.rmempty = true
-
-o.net.mac_hints(function(t, a) e:value(t, "%s (%s)" % {t, a}) end)
-
-e = t:option(Value, "timeon", translate("Start time"))
-e.placeholder = "00:00"
-e.rmempty = true
-
-e = t:option(Value, "timeoff", translate("End time"))
-e.placeholder = "23:59"
 e.rmempty = true
 
 e = t:option(Value, "description", translate("Description"))
