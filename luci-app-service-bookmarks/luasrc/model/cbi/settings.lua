ca10ec958d89afbf2c7f408749e55fd75b29233c
sveil <support@sveil.com>
2025-05-13 11:01:01 +0800

    fix service_bookmarks #14

--- luci-app-service-bookmarks/luasrc/model/cbi/settings.lua
+++ luci-app-service-bookmarks/luasrc/model/cbi/settings.lua
@@ -1,7 +1,7 @@
 local o = require "luci.sys"
 
 a = Map("service_bookmarks", translate("Service BookMarks"), translate("Set service bookmarks here, which can include any service elements like title, url, description..."))
-a.template = "service_bookmarks/index"
+a.template = "service_bookmarks/settings"
 
 t = a:section(TypedSection, "service", translate("Service Settings"), translate("Set service bookmarks here, which can include any service elements like title, url, description..."))
 t.template = "cbi/tblsection"
