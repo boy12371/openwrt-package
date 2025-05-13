3b6ce79548887f6a2b93a3c34942b8fb92ceeedc
sveil <support@sveil.com>
2025-05-13 17:36:46 +0800

    fix service_bookmarks #16

--- luci-app-service-bookmarks/luasrc/controller/service_bookmarks.lua
+++ luci-app-service-bookmarks/luasrc/controller/service_bookmarks.lua
@@ -9,33 +9,4 @@ function index()
 
 	entry({ "admin", "services", "service_bookmarks", "home" }, cbi("home"), _("Home"), 20).leaf = true
 	entry({ "admin", "services", "service_bookmarks", "settings" }, cbi("settings"), _("Settings"), 30).leaf = true
-
-	-- 服务管理API
-	-- entry({"admin", "services", "service_bookmarks", "services"}, call("action_list_services")).leaf = true
-	-- entry({"admin", "services", "service_bookmarks", "services"}, call("action_add_service")).leaf = true
-	-- entry({"admin", "services", "service_bookmarks", "services", ":id"}, call("action_update_service")).leaf = true
-	-- entry({"admin", "services", "service_bookmarks", "services", ":id"}, call("action_delete_service")).leaf = true
-	entry({ "admin", "services", "service_bookmarks", "logtail" }, call("action_logtail")).leaf = true -- 日志采集
-    entry({ "admin", "services", "service_bookmarks", "status" }, call("status")).leaf = true -- 服务状态
-end
-
-function status()
-    local e = {}
-    e.status = luci.sys.call("iptables -L FORWARD | grep BOOKMARKS >/dev/null") == 0
-    luci.http.prepare_content("application/json")
-    luci.http.write_json(e)
-end
-
-function action_logtail()
-	local fs = require "nixio.fs"
-	local log_path = "/var/log/service_bookmarks.log"
-	local e = {}
-	e.running = luci.sys.call("pidof service_bookmarks >/dev/null") == 0
-	if fs.access(log_path) then
-		e.log = luci.sys.exec("tail -n 100 %s | sed 's/\\x1b\\[[0-9;]*m//g'" % log_path)
-	else
-		e.log = ""
-	end
-	luci.http.prepare_content("application/json")
-	luci.http.write_json(e)
 end
