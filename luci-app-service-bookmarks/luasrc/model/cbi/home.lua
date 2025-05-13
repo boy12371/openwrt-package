f26a8ee74a9173ab5cccfdf3b39316f3a011c4e6
sveil <support@sveil.com>
2025-05-13 19:56:49 +0800

    fix service_bookmarks #19

--- luci-app-service-bookmarks/luasrc/model/cbi/home.lua
+++ luci-app-service-bookmarks/luasrc/model/cbi/home.lua
@@ -2,7 +2,7 @@ a = Map("service_bookmarks", translate("Service Bookmarks"), translate("List all
 a.template = "service_bookmarks/home"
 a.pageaction = false
 
-t = a:section(TypedSection, "service", translate("Service Bookmarks"), translate("List all available service bookmarks here""))
+t = a:section(TypedSection, "service", translate("Service Bookmarks"), translate("List all available service bookmarks here"))
 t.template = "cbi/tblsection"
 t.anonymous = true
 t.addremove = false
