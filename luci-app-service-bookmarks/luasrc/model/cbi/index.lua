faf95be63a3cd083cab48efaea984eea7418a381
sveil <support@sveil.com>
2025-05-17 08:20:32 +0800

    fix service_bookmarks #28

--- luci-app-service-bookmarks/luasrc/model/cbi/index.lua
+++ luci-app-service-bookmarks/luasrc/model/cbi/index.lua
@@ -10,11 +10,11 @@ tabs:tab("home", translate("Home"))
 tabs:tab("settings", translate("Settings"))
 
 -- 首页选项卡内容
-eh = tabs:option("home", DummyValue, "title", translate("Service Title"))
+eh = tabs:taboption("home", DummyValue, "title", translate("Service Title"))
 eh.width = "15%"
 eh.rawhtml = false
 
-eh = tabs:option("home", DummyValue, "url", translate("Url Address"))
+eh = tabs:taboption("home", DummyValue, "url", translate("Url Address"))
 eh.width = "30%"
 eh.rawhtml = true
 function eh.cfgvalue(self, section)
@@ -22,20 +22,20 @@ function eh.cfgvalue(self, section)
     return string.format('<a href="%s" target="_blank">%s</a>', url, url)
 end
 
-eh = tabs:option("home", DummyValue, "description", translate("Description"))
+eh = tabs:taboption("home", DummyValue, "description", translate("Description"))
 eh.width = "45%"
 eh.rawhtml = false
 
 -- 设置选项卡内容
-es = tabs:option("settings", Value, "title", translate("Service Title"))
+es = tabs:taboption("settings", Value, "title", translate("Service Title"))
 es.width = "15%"
 es.rmempty = true
 
-es = tabs:option("settings", Value, "url", translate("Url Address"))
+es = tabs:taboption("settings", Value, "url", translate("Url Address"))
 es.width = "30%"
 es.rmempty = true
 
-es = tabs:option("settings", Value, "description", translate("Description"))
+es = tabs:taboption("settings", Value, "description", translate("Description"))
 es.width = "45%"
 es.rmempty = false
 
