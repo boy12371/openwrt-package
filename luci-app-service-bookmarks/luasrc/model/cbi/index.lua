35c0293a1014d9d4c048caebbedff72a9009831b
sveil <support@sveil.com>
2025-05-19 13:23:32 +0800

    fix service_bookmarks #40

--- luci-app-service-bookmarks/luasrc/model/cbi/index.lua
+++ luci-app-service-bookmarks/luasrc/model/cbi/index.lua
@@ -1,111 +1,268 @@
-m = Map("service_bookmarks", "KikiAuth", translate("KikiAuth creates a captive portal to work with WifiDog. KikiAuth support logging in with popular OAuth services account.")) -- We want to edit the uci config file /etc/config/kikiauth
+-- Copyright 2017 Harry Gabriel (rootdesign@gmail.com)
+-- Copyright 2018 Anton Kikin (a.kikin@tano-systems.com)
+-- This is free software, licensed under the Apache License, Version 2.0
 
-s = m:section(NamedSection, "facebook", "oauth_services", "Facebook",
-              translate("You can register your own Facebook app and use its parameters here."))
+local ttydcfg = "/etc/config/ttyd"
+local fs = require("nixio.fs")
 
-s:tab("general", translate("General settings"))
-s:tab("ip", translate("Service IP addresses"))
+local m, s, o
 
-e = s:taboption("general", SerFlag, "enabled", translate("Enabled?"))
+m = Map("ttyd", translate("Terminal settings"))
 
----***---
-p = s:taboption("general", Value, "app_id", "App ID/ Client ID",
-                translate("The App ID/API Key of your <a href='https://developers.facebook.com/apps'>\
-                          registered Facebook app</a>."))
-p:depends('enabled', '1')
-p.default = '420756987974770'
-p = s:taboption("general", Value, "redirect_uri", "Redirect URI",
-             translate("This URI has to be match the one you registered for your Facebook app."))
-p:depends('enabled', '1')
-p.default = 'http://openwrt.lan/cgi-bin/luci/kikiauth/oauth/facebookcallback'
+s = m:section(TypedSection, "server", translate("TTYd server settings"))
+s.anonymous = true
+s.addremove = false
 
----***---
-p = s:taboption("ip", IPList, "ips", "Facebook IPs",translate("List of Facebook IPs used for the gateway to open the traffic correctly while using Facebook OAuth."))
-p:depends('enabled', '1')
-p.default = {'www-slb-10-01-prn1.facebook.com',
-             'www-slb-11-12-prn1.facebook.com',
-             's-static.ak.fbcdn.net'}
+---------------------------
 
----***---
-p = s:taboption("ip", Flag, "check_enabled", translate("Periodically check and remove deprecated IP?"))
-p:depends('enabled', '1')
-p = s:taboption("ip", ListValue, "day", translate("Day"))
-p:depends('check_enabled', '1')
-local weekdays = {{'Sun', 'Sunday'},
-                  {'Mon', 'Monday'},
-                  {'Tue', 'Tuesday'},
-                  {'Wed', 'Wednesday'},
-                  {'Thu', 'Thursday'},
-                  {'Fri', 'Friday'},
-                  {'Sat', 'Saturday'},
-                  {'Every', 'Everyday'}}
-for _, d in ipairs(weekdays) do
-	p:value(d[1], translate(d[2]))
+s:tab("basic", translate("Basic Settings"))
+s:tab("advanced", translate("Advanced Settings"))
+s:tab("ssl", translate("SSL Settings"))
+s:tab("basic_auth", translate("Basic Authentication"))
+
+---------------------------
+
+-- [port, -p]
+-- Port to listen (default: 7681, use `0` for random port)
+o = s:taboption("basic", Value, "port",
+	translate("Port"),
+	translate("Port to listen (default: 7681)"))
+
+o.default = 7681
+o.datatype = "port"
+o.rmempty = true
+o.placeholder = 7681
+
+-- [interface, -i]
+-- Network interface to bind (eg: eth0), or UNIX domain socket path (eg: /var/run/ttyd.sock)
+o = s:taboption("basic", Value, "interface",
+	translate("Interface"),
+	translate("Listen only on the given interface or, if unspecified, on all"))
+
+o.default = nil
+o.template = "cbi/network_netlist"
+o.nocreate = true
+o.unspecified = true
+o.nobridges = false
+
+---------------------------
+
+-- [readonly, -R]
+-- Do not allow clients to write to the TTY
+o = s:taboption("advanced", Flag, "readonly",
+	translate("Read only"),
+	translate("Do not allow clients to write to the TTY"))
+
+o.rmempty = true
+o.default = false
+o.optional = false
+
+-- [check-origin, -O]
+-- Do not allow websocket connection from different origin
+o = s:taboption("advanced", Flag, "check_origin",
+	translate("Check origin"),
+	translate("Do not allow websocket connection from different origin"))
+
+o.rmempty = true
+o.default = false
+o.optional = false
+
+-- [max-clients, -m]
+-- Maximum clients to support (default: 0, no limit)
+o = s:taboption("advanced", Value, "max_clients",
+	translate("Maximum clients"),
+	translate("Maximum clients to support (default: 0, no limit)"))
+
+o.placeholder = 0
+o.default = 0
+
+-- [debug, -d]
+-- Set log level (default: 7)
+o = s:taboption("advanced", MultiValue, "debug",
+	translate("Message types for logging"),
+	translate("Select websockets library message types for logging (default: LLL_ERR and LLL_WARN)"))
+
+o.default = "1 2"
+o.delimiter = " "
+o.widget = "checkbox"
+o.size = 11
+
+o:value(   1, translate("LLL_ERR"))
+o:value(   2, translate("LLL_WARN"))
+o:value(   4, translate("LLL_NOTICE"))
+o:value(   8, translate("LLL_INFO"))
+o:value(  16, translate("LLL_DEBUG"))
+o:value(  32, translate("LLL_HEADER"))
+o:value( 128, translate("LLL_EXT"))
+o:value( 256, translate("LLL_CLIENT"))
+o:value( 512, translate("LLL_LATENCY"))
+o:value(1024, translate("LLL_USER"))
+o:value(2048, translate("LLL_THREAD"))
+
+---------------------------
+
+local ssl = nil
+local ssl_cert = nil
+local ssl_key = nil
+
+-- [ssl, -S]
+-- Enable SSL
+ssl = s:taboption("ssl", Flag, "ssl",
+	translate("Enable SSL"))
+
+function ssl.validate(self, value, section)
+	if ssl and ssl:formvalue(section) and (#(ssl:formvalue(section)) > 0) then
+		if ((not ssl_cert) or
+		    (not ssl_cert:formvalue(section)) or
+		    (ssl_cert:formvalue(section) == "")) then
+			return nil, translate("Must have certificate when using SSL")
+		end
+		
+		if ((not ssl_key) or
+		    (not ssl_key:formvalue(section)) or
+		    (ssl_key:formvalue(section) == "")) then
+			return nil, translate("Must have key when using SSL")
+		end
+	end
+
+	return value
 end
-p = s:taboption("ip", ListValue, "time", translate("Time"))
-p:depends('check_enabled', '1')
-for i = 0, 23 do
-	p:value("%02d" % {i}, tostring(i))
+
+-- [ssl-cert, -C]
+-- SSL certificate file path
+ssl_cert = s:taboption("ssl", FileUpload, "ssl_cert",
+	translate("HTTPS certificate (PEM&nbsp;format)"),
+	translate("SSL certificate file path"))
+	
+ssl_cert:depends("ssl", 1)
+ssl_cert.default = '/etc/ttyd/ttyd.crt'
+
+-- [ssl-key, -K]
+-- SSL key file path
+ssl_key = s:taboption("ssl", FileUpload, "ssl_key",
+	translate("HTTPS private key (PEM&nbsp;format)"),
+	translate("SSL key file path"))
+	
+ssl_key:depends("ssl", 1)
+ssl_key.default = '/etc/ttyd/ttyd.key'
+
+o = s:taboption("ssl", Button, "remove_ssl_cert_and_key",
+	translate("Remove certificate and key"),
+	translate("TTYd will generate a new self-signed certificate using the configuration shown below"))
+
+o.inputstyle = "remove"
+o:depends("ssl", 1)
+
+function o.write(self, section)
+	if ssl_cert:cfgvalue(section) and fs.access(ssl_cert:cfgvalue(section)) then
+		fs.unlink(ssl_cert:cfgvalue(section))
+	end
+
+	if ssl_key:cfgvalue(section) and fs.access(ssl_key:cfgvalue(section)) then
+		fs.unlink(ssl_key:cfgvalue(section))
+	end
+
+	luci.sys.call("/etc/init.d/ttyd restart")
+	luci.http.redirect(luci.dispatcher.build_url("admin", "system", "terminal", "configure"))
 end
 
----***---
-s = m:section(NamedSection, "google", "oauth_services", "Google",
-              translate("You can register your own Google app and use its parameters here."))
-s:tab("general", translate("General settings"))
-s:tab("ip", translate("Service IP addresses"))
+---------------------------
 
-s:taboption("general", SerFlag, "enabled", translate("Enabled?"))
+-- [credential, -c]
+-- Credential for Basic Authentication (format: username:password)
+o = s:taboption("basic_auth", Flag, "credential",
+	translate("Use basic authentication"),
+	translate("Credentials for basic authentication"))
 
-p = s:taboption("general", Flag, "googleall", translate("Accept all Google accounts?"),
-                translate("User can use any Google Account email to login.<br/>"))
-p:depends('enabled', '1')
+o.default = 0
 
-p = s:taboption("general", DynamicList, "googleapps", translate("Accept these Google Apps domains"),
-            translate("Ex. <i>mbm.vn</i>. User will login with <i>@mbm.vn</i> email.<br />\
-                      <b>Note</b>: To limit to these domains, you have to uncheck the\
-                      <i>all Google account</i> option above."))
-p:depends('enabled', '1')
+o = s:taboption("basic_auth", Value, "username",
+	translate("Username"),
+	translate("Username for basic authentication"))
 
-p = s:taboption("general", Value, "app_id", "App ID/ Client ID",
-                translate("The Client ID of your app registered in\
-                          <a href='https://code.google.com/apis/console/'>Google API Console</a>"))
-p:depends('enabled', '1')
-p.default = '242929894222-3909mjqkmgcdo9ro6mr91aiod083g834.apps.googleusercontent.com'
+o.rmempty = true
+o:depends("credential", 1)
 
-p = s:taboption("general", Value, "redirect_uri", "Redirect URI",
-             translate("This URI has to be match the one you registered for your Google app.<br/>\
-             Have to be HTTPS. Its domain/IP must be included in <i>Service IP addresses</i> list (next tab)."))
-p:depends('enabled', '1')
-p.default = 'https://kikiauth.appspot.com/google'
+o = s:taboption("basic_auth", Value, "password",
+	translate("Password"),
+	translate("Password for basic authentication"))
 
-p = s:taboption("ip", IPList, "ips", "Google IPs",translate("List of Google IPs used for the gateway to open the traffic correctly while using Google OAuth."))
-p:depends('enabled', '1')
-p.default = {'accounts.l.google.com',
-             'accounts-cctld.l.google.com',
-             'clients.l.google.com',
-             'googlehosted.l.googleusercontent.com',
-             'ssl.gstatic.com',
-             'kikiauth.appspot.com'}
+o.password = true
+o.rmempty = true
+o:depends("credential", 1)
 
-p = s:taboption("ip", Flag, "check_enabled", translate("Periodically check and remove deprecated IP?"))
-p:depends('enabled', '1')
+---------------------------
 
-p = s:taboption("ip", ListValue, "day", translate("Day"))
-p:depends('check_enabled', '1')
-for _, d in ipairs(weekdays) do
-	p:value(d[1], translate(d[2]))
-end
+s = m:section(TypedSection, "ssl",
+	translate("Self-signed SSL certificate parameters"))
 
-p = s:taboption("ip", ListValue, "time", translate("Time"))
-p:depends('check_enabled', '1')
-for i = 0, 23 do
-	p:value("%02d" % {i}, tostring(i))
-end
+s.template  = "cbi/tsection"
+s.anonymous = true
 
-s = m:section(NamedSection, "oauth_success_page", "success_page", "Success page",
-              translate("You can set a default success page which users will be redirected to after logging in successfully. Or, you can just display some welcome text to these users; but notice that this text is only showed if you do not provide the 'Success URL' field a value."))
-p = s:option(Value, "success_url", "Success URL")
-p = s:option(TextValue, "success_text", "Success Text",translate("This is only displayed if you leave the 'Success URL' field blank. HTML tags can be used here."))
-p.rows = 6
+
+o = s:option(Value, "days",
+	translate("Number of days a generated certificate is valid"))
+
+o.default = 730
+o.placeholder = 730
+o.datatype = "uinteger"
+
+
+o = s:option(Value, "bits",
+	translate("Length of the private key in bits"))
+
+o.default = 2048
+o.datatype = "min(1024)"
+o.placeholder = 2048
+
+
+o = s:option(Value, "country",
+	translate("Country name (/C)"),
+	translate("The two-letter country code where your company is legally located"))
+
+o.default = "RU"
+o.placeholder = "RU"
+
+
+o = s:option(Value, "state",
+	translate("State or province name (/ST)"),
+	translate("The state/province where your company is legally located"))
+
+o.default = ""
+o.placeholder = "Saint-Petersburg"
+
+
+o = s:option(Value, "locality",
+	translate("Locality name (/L)"),
+	translate("The city where your company is legally located"))
+
+o.default = ""
+o.placeholder = "Saint-Petersburg"
+
+
+o = s:option(Value, "organization",
+	translate("Organization name (/O)"),
+	translate("Your company's legally registered name"))
+
+o.default = ""
+o.placeholder = "Your Company, Inc."
+
+
+o = s:option(Value, "organizational_unit",
+	translate("Organizational unit name (/OU)"),
+	translate("The name of your department within the organization"))
+
+o.default = ""
+o.placeholder = "IT department"
+
+
+o = s:option(Value, "commonname",
+	translate("Common name (/CN)"),
+	translate("The fully-qualified domain name"))
+
+o.default = luci.sys.hostname()
+o.placeholder = "www.example.com"
+
+---------------------------
 
 return m
