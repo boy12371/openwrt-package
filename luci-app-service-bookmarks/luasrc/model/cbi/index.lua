2be8c7b1c121578910a74f6f894c66be13b8f7b4
sveil <support@sveil.com>
2025-05-19 20:41:24 +0800

    fix service_bookmarks #43

--- luci-app-service-bookmarks/luasrc/model/cbi/index.lua
+++ luci-app-service-bookmarks/luasrc/model/cbi/index.lua
@@ -1,54 +1,52 @@
-local a, tabs, e, eh, es
+-- 1. 创建主Map对象
+local map = Map("service_bookmarks", translate("Service Bookmarks"), translate("List all available service bookmarks here"))
+map.pageaction = true
 
-a = Map("service_bookmarks", translate("Service Bookmarks"), translate("List all available service bookmarks here"))
-a.pageaction = true
+-- 2. 创建选项卡容器
+local tabs = map:add(Tabs("service_bookmarks_tabs"))
 
--- 创建选项卡容器
-tabs = a:section(TypedSection, "service", translate("Service Bookmarks"))
-tabs.anonymous = true
-tabs:tab("home", translate("Home"))
-tabs:tab("settings", translate("Settings"))
+-- 3. 首页选项卡
+local tab_home = tabs:add(Tab("home", translate("Home")))
+local section_home = tab_home:add(TypedSection, "home", translate("Service Bookmarks"))
+section_home.template = "cbi/tblsection"
+section_home.anonymous = true
+section_home.addremove = false
 
--- 首页选项卡内容
-e = a:section(TypedSection, "home", translate("Service Bookmarks"))
-e.template = "cbi/tblsection"
-e.anonymous = true
-e.addremove = false
+-- 4. 首页内容定义
+local title_home = section_home:option(DummyValue, "title", translate("Service Title"))
+title_home.default = ""
+title_home.width = "15%"
 
-eh = e:option(DummyValue, "title", translate("Service Title"))
-eh.default = ""
-eh.width = "15%"
-eh.rawhtml = false
-
-eh = e:option(DummyValue, "url", translate("Url Address"))
-eh.width = "30%"
-eh.rawhtml = true
-function e.cfgvalue(self, section)
+local url_home = section_home:option(DummyValue, "url", translate("Url Address"))
+url_home.width = "30%"
+url_home.rawhtml = true
+function url_home.cfgvalue(self, section)
     local url = self.map:get(section, "url") or ""
     return string.format('<a href="%s" target="_blank">%s</a>', url, url)
 end
 
-eh = e:option(DummyValue, "description", translate("Description"))
-eh.default = ""
-eh.width = "45%"
-eh.rawhtml = false
+local desc_home = section_home:option(DummyValue, "description", translate("Description"))
+desc_home.default = ""
+desc_home.width = "45%"
 
--- 设置选项卡内容
-e = a:section(TypedSection, "settings", translate("Service Settings"))
-e.template = "cbi/tblsection"
-e.anonymous = true
-e.addremove = true
+-- 5. 设置选项卡
+local tab_settings = tabs:add(Tab("settings", translate("Settings")))
+local section_settings = tab_settings:add(TypedSection, "settings", translate("Service Settings"))
+section_settings.template = "cbi/tblsection"
+section_settings.anonymous = true
+section_settings.addremove = true
 
-es = e:option(Value, "title", translate("Service Title"))
-es.width = "15%"
-es.rmempty = true
+-- 6. 设置内容定义
+local title_setting = section_settings:option(Value, "title", translate("Service Title"))
+title_setting.width = "15%"
+title_setting.rmempty = true
 
-es = e:option(Value, "url", translate("Url Address"))
-es.width = "30%"
-es.rmempty = true
+local url_setting = section_settings:option(Value, "url", translate("Url Address"))
+url_setting.width = "30%"
+url_setting.rmempty = true
 
-es = e:option(Value, "description", translate("Description"))
-es.width = "45%"
-es.rmempty = false
+local desc_setting = section_settings:option(Value, "description", translate("Description"))
+desc_setting.width = "45%"
+desc_setting.rmempty = false
 
-return a
+return map
