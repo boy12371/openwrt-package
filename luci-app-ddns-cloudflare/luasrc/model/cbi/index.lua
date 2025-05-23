a55c949f60ce8f27210b43aeb685638f343c346e
sveil <support@sveil.com>
2025-05-23 22:33:19 +0800

    fix ddns_cloudflare #1

--- luci-app-ddns-cloudflare/luasrc/model/cbi/index.lua
+++ luci-app-ddns-cloudflare/luasrc/model/cbi/index.lua
@@ -1,17 +1,23 @@
-local a = Map("ddns_cloudflare", translate("DDNS Cloudflare"), "")
+local a = Map("ddns_cloudflare", translate("DDNS Cloudflare"))
 a.template = "ddns_cloudflare/index"
 a.pageaction = false
 
-local t = a:section(TypedSection, "base", "", translate("List all available service bookmarks here"))
-t.template = "cbi/tblsection"
-t.anonymous = true
-t.addremove = false
+local t = m:section(NamedSection, "base", nil, "Facebook",
+              translate("You can register your own Facebook app and use its parameters here."))
 
-local e = t:option(DummyValue, "enabled", translate("Service Title"))
+-- local t = a:section(TypedSection, "base", "", translate("List all available service bookmarks here"))
+-- t.template = "cbi/tblsection"
+-- t.anonymous = true
+-- t.addremove = false
+
+local e = t:option(Value, "enabled", translate("Service Title"))
 e.default = ""
 e.rawhtml = false
 
-e = t:option(DummyValue, "cloudflare_sn", translate("Url Address"))
+e = t:option(Value, "interval", translate("Url Address"))
+e.rawhtml = true
+
+e = t:option(Value, "cloudflare_sn", translate("Url Address"))
 e.rawhtml = true
 
 return a
