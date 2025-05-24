d14b9b8c235c39947ab1588ffe0ee5b932f1c0af
sveil <support@sveil.com>
2025-05-24 21:47:44 +0800

    fix ddns_cloudflare #3

--- luci-app-ddns-cloudflare/luasrc/model/cbi/index.lua
+++ luci-app-ddns-cloudflare/luasrc/model/cbi/index.lua
@@ -2,22 +2,34 @@ local a = Map("ddns_cloudflare", translate("DDNS Cloudflare"))
 a.template = "ddns_cloudflare/index"
 a.pageaction = false
 
-local t = a:section(NamedSection, "base", nil, "Facebook",
-              translate("You can register your own Facebook app and use its parameters here."))
+local t = a:section(NamedSection, "base", nil, "Cloudflare Settings",
+              translate("Configure your Cloudflare DDNS settings here"))
 
 -- local t = a:section(TypedSection, "base", "", translate("List all available service bookmarks here"))
 -- t.template = "cbi/tblsection"
 -- t.anonymous = true
 -- t.addremove = false
 
-local e = t:option(Value, "enabled", translate("Service Title"))
-e.default = ""
-e.rawhtml = false
+local e = t:option(Flag, "enabled", translate("Enable"), 
+              translate("Enable Cloudflare DDNS service"))
+e.default = "0"
+e.rmempty = false
 
-e = t:option(Value, "interval", translate("Url Address"))
-e.rawhtml = true
+e = t:option(Value, "interval", translate("Update Interval"), 
+              translate("Time interval in minutes between updates"))
+e.datatype = "uinteger"
+e.default = "10"
 
-e = t:option(Value, "cloudflare_sn", translate("Url Address"))
-e.rawhtml = true
+e = t:option(Value, "api_key", translate("API Key"), 
+              translate("Your Cloudflare Global API Key"))
+e.password = true
+
+e = t:option(Value, "zone_name", translate("Zone Name"), 
+              translate("The domain name to update (e.g. example.com)"))
+e.datatype = "hostname"
+
+e = t:option(Value, "record_name", translate("Record Name"), 
+              translate("DNS record to update (e.g. www)"))
+e.datatype = "hostname"
 
 return a
