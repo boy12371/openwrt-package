161fe3f0a4ceb446113877ac2998181f8ac12233
sveil <support@sveil.com>
2025-05-25 14:02:00 +0800

    fix ddns_cloudflare #5

--- luci-app-ddns-cloudflare/luasrc/model/cbi/index.lua
+++ luci-app-ddns-cloudflare/luasrc/model/cbi/index.lua
@@ -1,35 +1,27 @@
-local a = Map("ddns_cloudflare", translate("DDNS Cloudflare"))
+local a = Map("ddns_cloudflare", translate("DDNS Cloudflare Configuration"))
 a.template = "ddns_cloudflare/index"
-a.pageaction = false
 
-local t = a:section(NamedSection, "base", "ddns", "Cloudflare Settings",
-              translate("Configure your Cloudflare DDNS settings here"))
+local t = a:section(NamedSection, "base", "ddns", translate("Cloudflare Settings"))
+t.anonymous = true
 
--- local t = a:section(TypedSection, "base", "", translate("List all available service bookmarks here"))
--- t.template = "cbi/tblsection"
--- t.anonymous = true
--- t.addremove = false
-
-local e = t:option(Flag, "enabled", translate("Enable"), 
-              translate("Enable Cloudflare DDNS service"))
+local e = t:option(Flag, "enabled", translate("Enable"), translate("Enable Cloudflare DDNS service"))
 e.default = "0"
-e.rmempty = false
 
-e = t:option(Value, "interval", translate("Update Interval"), 
-              translate("Time interval in minutes between updates"))
+e = t:option(Value, "interval", translate("Update Interval"), translate("Time in minutes between DNS updates"))
 e.datatype = "uinteger"
 e.default = "10"
+e:depends("enabled", "1")
 
-e = t:option(Value, "api_key", translate("API Key"), 
-              translate("Your Cloudflare Global API Key"))
+e = t:option(Value, "api_key", translate("API Key"), translate("Cloudflare Global API Key"))
 e.password = true
+e:depends("enabled", "1")
 
-e = t:option(Value, "zone_name", translate("Zone Name"), 
-              translate("The domain name to update (e.g. example.com)"))
+e = t:option(Value, "zone_name", translate("Zone Name"), translate("The domain name registered with Cloudflare"))
 e.datatype = "hostname"
+e:depends("enabled", "1")
 
-e = t:option(Value, "record_name", translate("Record Name"), 
-              translate("DNS record to update (e.g. www)"))
+e = t:option(Value, "record_name", translate("Record Name"), translate("The DNS record to update (e.g. 'home' for home.example.com)"))
 e.datatype = "hostname"
+e:depends("enabled", "1")
 
 return a
